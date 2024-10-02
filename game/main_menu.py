c383f2b41da89ff307ab6c309e36c6289a59c35f
BoredDany <martinezq.d@javeriana.edu.co>
2024-10-01 22:50:43 -0500

    MENU VISUALMENTE MEJOR

--- game/main_menu.py
+++ game/main_menu.py
@@ -2,10 +2,11 @@ import pygame
 from game.button import Button
 from game.game_screen import game_screen
 from game.development_screen import development_screen
+from game.screen_features import WIDTH, HEIGHT, WHITE, BLACK, GREEN, RED
 
 def main_menu(screen):
     pygame.init()
-    font = pygame.font.Font(None, 36)
+    font = pygame.font.Font(None, 30)
     
     def start_game():
         game_screen(screen)
@@ -14,15 +15,26 @@ def main_menu(screen):
         development_screen(screen)
     
     # Crear botones
+    button_width = 200
+    button_height = 50
     buttons = [
-        Button(200, 200, 200, 50, "JUGAR", font, (0, 0, 255), start_game),
-        Button(200, 300, 200, 50, "MAQUINA JUEGA", font, (255, 0, 0), show_development)
+        Button((WIDTH - button_width) // 2, HEIGHT // 2 + 20, button_width, button_height, "HUMANO JUEGA", font, GREEN, start_game),
+        Button((WIDTH - button_width) // 2, HEIGHT // 2 + 90, button_width, button_height, "MAQUINA JUEGA", font, RED, show_development)
     ]
 
     running = True
     while running:
-        screen.fill((0, 0, 0))  
-        
+        screen.fill(WHITE)
+        font_title = pygame.font.Font(None, 64)
+        font_subtitle = pygame.font.Font(None, 36)
+
+        # Título y subtítulo
+        title = font_title.render("¡Bienvenidos!", True, BLACK)
+        subtitle = font_subtitle.render("Selecciona tipo de jugador", True, BLACK)
+
+        screen.blit(title, (WIDTH // 2 - title.get_width() // 2, HEIGHT // 4))
+        screen.blit(subtitle, (WIDTH // 2 - subtitle.get_width() // 2, HEIGHT // 3))
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
 from game.main_menu import main_menu
+from game.screen_features import WIDTH, HEIGHT
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((600, 680))
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("El juego Hashiwokakero")
 
     main_menu(screen)
