1e292f8db646f84455ac190a46f8165a291a5589
BoredDany <martinezq.d@javeriana.edu.co>
2024-10-22 12:25:58 -0500

    falta arreglar la ruta del archivo

--- src/Main.py
+++ src/Main.py
@@ -2,7 +2,6 @@ from board import Board, Node
 from file_persistence import FilePersistence
 from game import Game
 from player import Player
-import os
 
 
 class Main:
@@ -32,9 +31,5 @@ class Main:
 
 # Example usage
 if __name__ == "__main__":
-    # Construct the relative path to the nodes.txt file
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(base_dir, "../assets/files/nodes.txt")
-    
-    main = Main(file_path, "Alice")
+    main = Main("nodes.txt", "Alice")
     main.run()
